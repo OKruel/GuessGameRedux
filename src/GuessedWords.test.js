ae7b944cc9c6df657b4e8f885cf5879dfc3d05b9
olavokruel <olavokruel@gmail.com>
2019-09-10 18:48:04 -0300

    Started tests for GuessedWords

---
+++ src/GuessedWords.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { findByTestAttr, checkProps } from '../test/testUtils'
+import GuessedWords from './GuessedWords'
+
+//!Setup of the tests
+const defaultProps = {
+    guessedWords: [
+        { guessedWord: 'train', letterMatchCount: 3 }
+    ]
+}
+const setup = (props = {}) => {
+    const setupProps = { ...defaultProps, ...props }
+    return shallow(<GuessedWords {...setupProps} />)
+}
+//!Tests Begining
+test('does not throw a warning with expected props', () => {
+    checkProps(GuessedWords, defaultProps)
+})
+
+describe('if there are no words guessed', () => {
+    let wrapper
+    beforeEach(() => {
+        wrapper = setup({ guessedWords: [] })
+    })
+
+    test('renders without errors', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words')
+        expect(component.length).toBe(1)
+    })
+    test('render instructions to guess a word', () => {
+        const instructions = findByTestAttr(wrapper, 'guess-instructions')
+        expect(instructions.text()).not.toBe(0)
+    })
+})
+
+describe('if there are words guessed', () => {
+
+})
