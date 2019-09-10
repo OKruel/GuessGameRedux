650e941514668a3623ac14b9a6d38383e2d2d19e
olavokruel <olavokruel@gmail.com>
2019-09-10 20:08:56 -0300

    Initiated Redux state

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import success from './successReducer'
+
+const rootReducer = combineReducers(
+    {
+        success,
+    }
+)
+
+export default rootReducer
