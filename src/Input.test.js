eb0112824841c597b14cfdba95232f571617073f
olavokruel <olavokruel@gmail.com>
2019-09-10 22:44:36 -0300

    Creating input.js and input.test.js

---
+++ src/Input.test.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { findByTestAtrr, storeFactory } from '../test/testUtils'
+import Input from './Input'
+
+const setup = (initialState={}) => {
+    const store = storeFactory(initialState)
+    const wrapper = shallow(<Input store={store}/>)
+    console.log(wrapper.debug())
+    return wrapper
+}
+setup()
+
+describe('renders', () => {
+    describe('word has NOT been guessed', () => {
+        test('renders component without error', () => {
+            
+            
+        })
+        test('renders input box', () => {
+
+        })
+        test('renders submit button', () => {
+
+        })
+    })
+
+    describe('word HAS been guessed', () => {
+        test('renders component without error', () => {
+            
+        })
+        test('DOES NOT renders input box', () => {
+
+        })
+        test('DOES NOT renders submit button', () => {
+
+        })
+    })
+
+})
+
+describe('update state', () => {
+
+})
